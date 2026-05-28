dffbbb0f86b885095c684316c4dfec30c0c3dc82
Proprince11 <hitraj1928@gmail.com>
2026-05-29 01:14:27 +0530

    Added Firebase config keys

---
+++ firebase.js
@@ -0,0 +1,20 @@
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCN90gGJYotvIFof1M7zxSSIdWzjWjZdB0",
+    authDomain: "voidscans-6c66b.firebaseapp.com",
+    projectId: "voidscans-6c66b",
+    storageBucket: "voidscans-6c66b.firebasestorage.app",
+    messagingSenderId: "1075674977587",
+    appId: "1:1075674977587:web:f306a9e3dd0de2ec5e5a46"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
