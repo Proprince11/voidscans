152643b9568bfee8a59d443682adb425f942ff51
Proprince11 <hitraj1928@gmail.com>
2026-05-29 08:12:49 +0530

    Fixed Firebase configuration and removed HTML tags

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCN90gGJYotvIFof1M7zxSSIdWzjWjZdB0",
+  authDomain: "voidscans-6c66b.firebaseapp.com",
+  projectId: "voidscans-6c66b",
+  storageBucket: "voidscans-6c66b.firebasestorage.app",
+  messagingSenderId: "1075674977587",
+  appId: "1:1075674977587:web:f306a9e3dd0de2ec5e5a46"
+};
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCN90gGJYotvIFof1M7zxSSIdWzjWjZdB0",
-    authDomain: "voidscans-6c66b.firebaseapp.com",
-    projectId: "voidscans-6c66b",
-    storageBucket: "voidscans-6c66b.firebasestorage.app",
-    messagingSenderId: "1075674977587",
-    appId: "1:1075674977587:web:f306a9e3dd0de2ec5e5a46"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and export it
+export const db = getFirestore(app);
